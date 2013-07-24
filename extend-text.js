ef46497f20d4878c148634a037d0744be72c32bc
Ryan Zec <code@ryanzec.com>
2013-07-24 07:13:59 -0400

    Added inline documentation Code clean up

--- extend-text.js
+++ extend-text.js
@@ -1,3 +1,12 @@
+/**
+ * Extend text component
+ *
+ * @module nag.extendText
+ * @ngdirective nagExtendText
+ *
+ * @nghtmlattribute {object} nag-extend-text Tells AngularJS this element is an extend text component and the passed object overwrite default for $scope.options
+ * @nghtmlattribute {object} data-model Data model to use for this component
+ */
 angular.module('nag.extendText', [
   'nag.core',
   'nag.beat',
@@ -24,6 +33,14 @@ angular.module('nag.extendText', [
       controller: [
         '$scope',
         function($scope) {
+          /**
+           * Sets the model controller
+           *
+           * @ngdirectivecontroller
+           * @method setModelController
+           *
+           * @param {object} modelController Model controller
+           */
           this.setModelController = function(modelController) {
             $scope.modelController = modelController;
           }
@@ -32,10 +49,14 @@ angular.module('nag.extendText', [
       templateUrl: 'components/nucleus-angular-extend-text/assets/templates/extend-text.html',
       transclude: true,
       compile: function(element, attributes, transclude) {
+        if(!attributes.id) {
+            throw new Error("Must provide data-id attribute for extend text");
+        }
         element.addClass('extend-text');
         return {
           pre: function(scope, element, attributes, controllers) {
             //add callback if this form is a resettable form
+            //todo: refactor: use events instead of callbacks
             if(controllers) {
               if(controllers[0]) {
                 controllers[0].addCallback(function() {
@@ -73,6 +94,65 @@ angular.module('nag.extendText', [
               element.find('.inputs').append($compile(displayElement)(scope)).append(hiddenElement);
             });
 
+            /**
+             * Options
+             *
+             * @ngscope
+             * @property {object} options
+             *   @property {string} [options.rootTemplatePath=rootTemplatePath+'/nucleus-angular-extend-text/assets/templates'] Root path for templates
+             *   @property {boolean} [options.selectOnFocus=false] Whether or not to select of data when focusing on input
+             *   @property {boolean} [options.preventSubmitOnEnter=true] Whether or not the prevent form submission on the enter key when input focused
+             *   @property {array} [options.data] The data for the input
+             *   @proeprty {boolean} [options.autoFocus=false] Whether
+             *   @property {string} [options.templateUrl="extend-text.html"] Main template url
+             *   @property {string} [options.template=""] Template HTML
+             */
+
+            /**
+             * Auto Complete Options
+             *
+             * @ngscope
+             * @property {object} options.autoCompleteOptions
+             *   @property {string} [options.autoCompleteOptions.rootTemplatePath=rootTemplatePath+'/nucleus-angular-extend-text/assets/templates']
+             *   Root path for templates
+             *   @property {boolean} [options.autoCompleteOptions.enabled=false] Whether or not auto complete functionality is enabled
+             *   @property {boolean} [options.autoCompleteOptions.display=false] Whether or not the auto complete is currently being displayed
+             *   @property {string} [options.autoCompleteOptions.url=null] Base url for where the auto complete data is retrieved from from
+             *   @property {string} [options.autoCompleteOptions.variable="input"] Variable name to use in the query string when retrieving the auto complete data
+             *   @property {string} [options.autoCompleteOptions.variableCache=null]
+             *   The last value sent when retrieving the auto complete data in order to prevent duplicate requests for the same data
+             *   @property {number} [options.autoCompleteOptions.loadCharacterCount=3]
+             *   Number of character needed to be enter before attempting to retrieve data for the auto complete
+             *   @property {number} [options.autoCompleteOptions.searchDelay=350]
+             *   Number of milliseconds to wait from last key entered to search for data to prevent unnecessary request if the user was not done typing there search
+             *   @property {cache} [options.autoCompleteOptions.cache=false]
+             *   Whether or not to cache the result from the server, useful for smaller data sets where the entire set can be easily cached and parsed on the javascript side
+             *   @property {array} [options.autoCompleteOptions.cachedData] Cached data is cache is set to true
+             *   @property {array} [options.autoCompleteOptions.options] Current set of auto complete options being displayed
+             *   @property {number} [options.autoCompleteOptions.selectedOptionIndex=0] Index (zero-based) or the currently selected option
+             *   @property {boolean} [options.autoCompleteOptions.selectOnBlur=true] Selected the currently higlighted option when blurring on the input
+             *   @property {boolean} [options.autoCompleteOptions.allowFreeForm=false]
+             *   Allow the user to enter in free form text (does not have to match something in the auto complete)
+             *   @property {function} [options.autoCompleteOptions.generateDataUrl]
+             *   Generate the URL used to retrieve the auto complete data form the remote service
+             *   @property {string} [options.autoCompleteOptions.remoteDataMethod="GET"] HTTP method to use when requesting data from the remote service
+             *   @property {boolean} [options.autoCompleteOptions.loadingData=false] Whether or not to display the loaded data message
+             *   @property {function} [options.autoCompleteOptions.responseParser] Function to parse the response when retrieving the auto complete data
+             *   @property {function} [options.autoCompleteOptions.formatVariable] Function used to format the variable when generating the URL
+             *   @property {function} [options.autoCompleteOptions.filter] Function to use to filter the data when cache is set to true
+             */
+
+            /**
+             * Tagging Options
+             *
+             * @ngscope
+             * @property {object} options.tagOptions
+             *   @property {string} [options.rootTemplatePath=rootTemplatePath+'/nucleus-angular-extend-text/assets/templates'] Root path for templates
+             *   @property {boolean} [options.tagOptions.enabled=false] Whether or not tagging is enabled
+             *   @property {boolean} [options.tagOptions.allowDuplicates=false] Whether or not to allow duplicate values
+             *   @property {number} [options.tagOptions.enabled=null] The index (zero-based) or the currently selected name
+             *   @property {boolean} [options.tagOptions.doubleClickEdit=false] Whether or not to enable tag editing on double click
+             */
             scope.options = nagDefaults.getExtendTextOptions(scope.options);
             var defaultAutoCompleteOptions = _.clone(scope.options.autoCompleteOptions.options);
             var beatName = 'extend-text-' + scope.$id;
@@ -185,8 +265,23 @@ angular.module('nag.extendText', [
               });
             };
 
+            /**
+             * Whether or not it is active
+             *
+             * @ngscope
+             * @property {boolean} isActive
+             */
             scope.isActive = false;
 
+            /**
+             * Add or set a new value for the input
+             *
+             * @ngscope
+             * @method newValue
+             *
+             * @param {mixed} display The value displayed to the user
+             * @param {mixed} [value=display] The value used for the hidden field (which might or might not differ from the display value)
+             */
             scope.newValue = function(display, value) {
               value = value || display;
 
@@ -203,6 +298,14 @@ angular.module('nag.extendText', [
               }
             };
 
+            /**
+             * Clear out the current input values
+             *
+             * @ngscope
+             * @method resetValues
+             *
+             * @param {boolean} [leaveDisplayValue=false] Whether or not to clear out the display value too
+             */
             scope.resetValues = function(leaveDisplayValue) {
               if(leaveDisplayValue !== true) {
                 $(element).find('input.display').val('');
@@ -211,6 +314,15 @@ angular.module('nag.extendText', [
               scope.options.data = [];
             }
 
+            /**
+             * Remove as value from the input
+             *
+             * @ngscope
+             * @method removeValue
+             *
+             * @param {mixed} value Value to remove
+             * @param {boolean} [focusTextArea=true] Whether or not to focus the on input when removing the value
+             */
             scope.removeValue = function(value, focusTextArea) {
               focusTextArea = focusTextArea || true;
               if(scope.options.tagOptions.enabled === true) {
@@ -226,6 +338,14 @@ angular.module('nag.extendText', [
               }
             };
 
+            /**
+             * Retrieve the value for the hidden input
+             *
+             * @ngscope
+             * @method getHiddenValue
+             *
+             * @returns {string} Hidden input value
+             */
             scope.getHiddenValue = function() {
               if(scope.options.tagOptions.enabled === true) {
                 return scope.options.data.length > 0 ? angular.toJson(scope.options.data) : '';
@@ -234,6 +354,14 @@ angular.module('nag.extendText', [
               }
             }
 
+            /**
+             * Retrieve the value for the displau input
+             *
+             * @ngscope
+             * @method getVisibleValue
+             *
+             * @returns {string} Display input value
+             */
             scope.getVisibleValue = function() {
               if(scope.options.tagOptions.enabled === true) {
                 return '';
@@ -242,35 +370,92 @@ angular.module('nag.extendText', [
               }
             };
 
+            /**
+             * Retrieve the value in the simulated text area
+             *
+             * @ngscope
+             * @method getTextAreaValue
+             *
+             * @returns {string} Simulate text area value
+             */
             scope.getTextAreaValue = function() {
               return $(element).find('input.display').val();
             };
 
             this.getTextAreaValue = scope.getTextAreaValue;
 
+            /**
+             * Whether or not the passed index is currently the selected tag
+             *
+             * @ngscope
+             * @method isSelectedTag
+             *
+             * @todo: refactor: rename key to index
+             * @param {number} key Number of the index (zero-based) to check for current selection
+             *
+             * @returns {boolean} Whether passed index is selected tag
+             */
             scope.isSelectedTag = function(key) {
               return key === scope.options.tagOptions.selectedTagIndex;
             };
 
+            /**
+             * Unselected any selected tag
+             *
+             * @ngscope
+             * @method resetSelectedTag
+             */
             scope.resetSelectedTag = function() {
               scope.options.tagOptions.selectedTagIndex = null;
             };
 
+            /**
+             * Whether or not the passed index is currently the selected auto complete option
+             *
+             * @ngscope
+             * @method isSelectedOption
+             *
+             * @todo: refactor: rename key to index
+             * @param {number} key Number of the index (zero-based) to check for current auto complete option
+             *
+             * @returns {boolean} Whether passed index is selected auto complete option
+             */
             scope.isSelectedOption = function(key) {
               return key === scope.options.autoCompleteOptions.selectedOptionIndex;
             };
 
+            /**
+             * Unselected any selected auto complete option
+             *
+             * @ngscope
+             * @method resetSelectedOption
+             */
             scope.resetSelectedOption = function() {
               scope.options.autoCompleteOptions.selectedOptionIndex = 0;
             };
 
+            /**
+             * Mouseup event handler
+             *
+             * @ngscope
+             * @method mouseUp
+             *
+             * @param {object} $event Event
+             */
             scope.mouseUp = function($event) {
               if(scope.options.selectOnFocus === true) {
                 $(element).find('input.display').select();
               }
             };
 
-            //handles preventing default actions for input
+            /**
+             * Keydown event handler
+             *
+             * @ngscope
+             * @method keyDown
+             *
+             * @param {object} $event Event
+             */
             scope.keyDown = function($event) {
               /*//handle prevent of enter submitted form
               if((scope.options.tagOptions.enabled === true || scope.options.preventSubmitOnEnter === true) && $event.which === 13) {
@@ -290,6 +475,14 @@ angular.module('nag.extendText', [
               }*/
             };
 
+            /**
+             * Keyup event handler
+             *
+             * @ngscope
+             * @method keyUp
+             *
+             * @param {object} $event Event
+             */
             scope.keyUp = function($event) {
               //handle prevent of enter submitted form
               if((scope.options.tagOptions.enabled === true || scope.options.preventSubmitOnEnter === true) && $event.which === 13) {
@@ -369,7 +562,7 @@ angular.module('nag.extendText', [
                 } else if(_.indexOf(noneAffectingTextKeys, $event.which) === -1) {
                   //using any character that affects text should reset the value if not allowing free form with auto complete
                   if(scope.options.autoCompleteOptions.allowFreeForm !== true) {
-                      scope.model[scope.modelController.$name] = '';
+                    scope.modelController.$setViewValue('');
                   }
 
                   //using a timeout so that the input value is the proper value when evaluating it
@@ -393,6 +586,14 @@ angular.module('nag.extendText', [
               }
             };
 
+            /**
+             * Blue event handler
+             *
+             * @ngscope
+             * @method blue
+             *
+             * @param {object} $event Event
+             */
             scope.blur = function($event) {
               if(scope.options.tagOptions.enabled === true) {
                 $(element).find('input.display').val('');
@@ -407,7 +608,7 @@ angular.module('nag.extendText', [
                   scope.newValue(newItem.display, newItem.value);
                 }
 
-                if(scope.options.autoCompleteOptions.allowFreeForm !== true && scope.model[scope.modelController.$name] == '') {
+                if(scope.options.autoCompleteOptions.allowFreeForm !== true && scope.$eval('model.' + scope.modelController.$name) == '') {
                   scope.resetValues();
                 }
 
@@ -418,6 +619,14 @@ angular.module('nag.extendText', [
               scope.isActive = false;
             };
 
+            /**
+             * Focus event handler
+             *
+             * @ngscope
+             * @method focus
+             *
+             * @param {object} $event Event
+             */
             scope.focus = function($event) {
               if(scope.options.autoCompleteOptions.enabled === true) {
                 scope.options.autoCompleteOptions.selectedOptionIndex = 0;
@@ -427,6 +636,14 @@ angular.module('nag.extendText', [
               scope.isActive = true;
             };
 
+            /**
+             * Double click event handler
+             *
+             * @ngscope
+             * @method doubleClick
+             *
+             * @param {object} $event Event
+             */
             scope.doubleClick = function($event, value) {
               if(scope.options.tagOptions.doubleClickEdit === true) {
                 scope.removeValue(value);
@@ -434,34 +651,45 @@ angular.module('nag.extendText', [
               }
             };
 
+            /**
+             * Click event handler for ul
+             *
+             * @ngscope
+             * @method ulClick
+             *
+             * @param {object} $event Event
+             */
             scope.ulClick = function($event) {
               $event.stopPropagation();
               element.find('input.display').focus();
             };
 
-            var hiddenValueSet = false;
+            /**
+             * Update visible/hidden inputs when data changes
+             *
+             * @ngwatch options.data
+             */
             scope.$watch('options.data', function(newValue, oldValue) {
               $timeout(function(){
-                if(scope.model) {
-                  scope.model[scope.modelController.$name] = scope.getHiddenValue();
-                }
-
                 var hiddenValue = scope.getHiddenValue();
-//                element.find('input[type="hidden"]').val(scope.getHiddenValue());
                 element.find('input[type="hidden"]').val(scope.getHiddenValue());
                 element.find('input.display').val(scope.getVisibleValue());
 
-                if(hiddenValue != '' && hiddenValueSet === true) {
+                if(hiddenValue != '') {
                   scope.modelController.$setViewValue(scope.getHiddenValue());
                 }
 
-                hiddenValueSet = true;
                 updateTextAreaPadding();
                 updateAutoCompletePosition();
                 //setElementHeight();
               }, 0);
             }, true);
 
+            /**
+             * Update auto complete position when auto complete options change
+             *
+             * @ngwatch options.autoCompleteOptions.options
+             */
             scope.$watch('options.autoCompleteOptions.options', function(newValue, oldValue) {
               $timeout(function(){
                 updateAutoCompletePosition();
@@ -469,6 +697,15 @@ angular.module('nag.extendText', [
             }, true);
 
             //setElementHeight();
+
+            /**
+             * Set the data when the set-data event is triggered for the extend box, event name uses the data-id attribute to listen to a unique event name
+             *
+             * @respondto extend-text-[data-id]::set-data
+             */
+            scope.$on('extend-text-' + attributes.id + '::set-data', function(self, data) {
+              scope.options.data = data;
+            });
           }
         };
       }
